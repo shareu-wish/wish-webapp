ba7a3f85455e4e42b8056218bf6977113798eacc
klims <klim.shchegolkov@mail.ru>
2024-05-11 22:33:37 +0300

    внёс изменения по дизайну старых страниц, поправил разводящую

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from flask import request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 @app.route('/home')
-def index():
-    return render_template("work.html")
+def home():
+    return render_template("klim's_test_logpage.html")
 
+@app.route('/main')
+def main():
+    return render_template("klim's_main.html")
 
+@app.route('/to_pay')
+def to_pay():
+    return render_template("klim's_bofore_pay.html")
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
