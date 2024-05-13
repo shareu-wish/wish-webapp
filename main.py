8e3bcc8ef8ef60f0ed72867bfd68263f4c50a758
ArtemMoshenko <91259325+ArtemMoshenko@users.noreply.github.com>
2024-05-13 10:40:09 +0300

    ФРОНТ_2
    Доделана часть фронта, все файлы html где есть есть _2 - это новые правильные

--- main.py
+++ main.py
@@ -6,18 +6,30 @@ from flask import request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form['text']
+        return render_template('registration_2.html', text=text)
+    else:
+        return render_template('registration_2.html')
+
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template("klim's_test_logpage.html")
-
-@app.route('/main')
+@app.route('/1')
 def main():
-    return render_template("klim's_main.html")
+    return render_template("admit_2.html")
 
-@app.route('/to_pay')
+
+@app.route('/')
+@app.route('/warning')
+def home():
+    return render_template("warning_2.html")
+
+
+@app.route('/registration')
 def to_pay():
-    return render_template("klim's_bofore_pay.html")
+    return render_template("registration_2.html")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
