6baff5a0da634f95f83b0fb280fda3e21aed9f4c
klims <klim.shchegolkov@mail.ru>
2024-06-04 01:07:32 +0300

    Новый скелет сайта

--- main.py
+++ main.py
@@ -1,41 +1,86 @@
 from flask import Flask
 from flask import render_template
-import json
-from flask import url_for
-from flask import request
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        return render_template('registration_2.html')
-    else:
-        return render_template('home_2.html')
-
-@app.route('/1', methods=['GET', 'POST'])
-def reg():
-    if request.method == 'POST':
-        text = request.form['text']
-        return render_template('registration_2.html', text=text)
-    else:
-        return render_template('registration_2.html')
-
-@app.route('/12')
-def main():
-    return render_template("home_2.html")
+application = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/warning')
-def home():
-    return render_template("warning_2.html")
+# ознакомительная страничка С КОМПАНИЕЙ
+@application.route('/')
+def landing():
+    return render_template("landing.html")
 
 
-@app.route('/registration')
-def to_pay():
-    return render_template("registration_2.html")
+# ознакомительная страничка С СЕРВИСОМ
+@application.route('/onboarding')
+def onboarding():
+    return render_template("onboarding.html")
+
+
+# авторизация (номер телефона/сервисы)
+@application.route('/auth')
+def auth():
+    return render_template("auth.html")
+
+
+# проверка номера телефона со станицы авторизации
+@application.route('/phone_check')
+def phone_check():
+    return render_template("phone_check.html")
+
+
+# аппараты в формате карточек
+@application.route('/devices_cards')
+def devices_cards():
+    return render_template("devises_cards.html")
+
+
+# аппараты на карте (Яндекс-карты)
+@application.route('/devices_map')
+def devices_map():
+    return render_template("devises_map.html")
+
+
+# ввод номера аппарата
+@application.route('/device_code')
+def device_code():
+    return render_template("device_code.html")
+
+
+# страница после оплаты депозита с номером ячейки зонта, фото и пожеланием
+@application.route('/get')
+def get_unbrella():
+    return render_template("get_unbrella.html")
+
+
+# страница после возвращения зонта с фото и ризывом возвращаться
+@application.route('/insert')
+def insert_umbrella():
+    return render_template("insert_umbrella.html")
+
+
+# личный кабинет пользователя с информацией о нём и статистикой
+@application.route('/account')
+def lk():
+    return render_template("lk.html")
+
+
+# личный кабинет пользователя с информацией о нём и статистикой
+@application.route('/support')
+def support():
+    return render_template("support.html")
+
+
+# служебная страничка на время разработки
+@application.route('/development')
+def development():
+    return render_template("pass.html")
+
+
+# служебная страничка на время разработки
+@application.route('/test')
+def test():
+    return render_template("tests.html")
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    application.run(port=8080, host='127.0.0.1')
