b988e311cdc2148c3a77fc9209cc910c9e14816d
Mark Meshcheryakov <markzum@yandex.ru>
2024-06-17 12:04:59 +0300

    Основная карта вставлена. Карточки станций в процессе

---
+++ static/js/device_map.js
@@ -0,0 +1,133 @@
+function toggleStationWindow(windowId) {
+  if (activeStationWindow === windowId) {
+    $(`#${windowId}Window`).hide();
+    activeStationWindow = null;
+    return;
+  }
+
+  if (activeStationWindow) {
+    $(`#${activeStationWindow}Window`).hide();
+  }
+  $(`#${windowId}Window`).show();
+  activeStationWindow = windowId;
+}
+
+async function initMap() {
+  await ymaps3.ready;
+
+  const {
+    YMap,
+    YMapDefaultSchemeLayer,
+    YMapDefaultFeaturesLayer,
+    YMapControls,
+    YMapMarker,
+  } = ymaps3;
+
+  const { YMapDefaultMarker } = await ymaps3.import(
+    "@yandex/ymaps3-markers@0.0.1"
+  );
+
+  const { YMapGeolocationControl } = await ymaps3.import(
+    "@yandex/ymaps3-controls@0.0.1"
+  );
+
+  const map = new YMap(document.getElementById("map"), {
+    location: {
+      center: [37.617698, 55.755864],
+      zoom: 12,
+    },
+    controls: ["routeButtonControl"],
+  });
+
+  map.addChild(new YMapDefaultSchemeLayer());
+  map.addChild(new YMapDefaultFeaturesLayer());
+
+  /*ymaps3.geolocation.getPosition().then((pos) => {
+          console.log(pos);
+        });*/
+
+  /*const markerElement = document.createElement("div");
+        markerElement.className = "icon-marker";
+        markerElement.innerText = "I'm marker!";
+  
+        const marker = new YMapMarker(
+          {
+            coordinates: [25.229762, 55.289311]
+          },
+          markerElement
+        );
+  
+        map.addChild(marker);*/
+
+  /*map.addChild(
+          new YMapDefaultMarker({
+            coordinates: [25.229762, 55.289311],
+            title: "Hello World!",
+            subtitle: "kind and bright",
+            color: "blue",
+          })
+        );*/
+
+  map.addChild(
+    // Using YMapControls you can change the position of the control
+    new YMapControls({ position: "top right" })
+      // Add the geolocation control to the map
+      .addChild(new YMapGeolocationControl({}))
+  );
+
+  document.getElementsByClassName("ymaps3x0--control-button")[0].click();
+
+  const devices = [
+    {
+      station_id: 1,
+      title: "Певческая капелла",
+      address: "г. Санкт-Петербург, Набережная реки Мойки, 20",
+      coords: [59.939936, 30.320801],
+      image: "/static/img/pass.jpg",
+      umbrellas_count: 5,
+    },
+    {
+      station_id: 2,
+      title: "Дом Таля",
+      address: "г. Санкт-Петербург, Невский проспект, 6",
+      coords: [59.936997, 30.314489],
+      image: "/static/img/pass.jpg",
+      umbrellas_count: 3,
+    },
+  ];
+
+  for (const device of devices) {
+    const markerElement = document.createElement("div");
+    markerElement.className = "device-marker";
+    markerElement.innerHTML = `
+      <div onclick="toggleStationWindow('${device.station_id}')">
+        <div class="umbrellas-count">${device.umbrellas_count}</div>
+        <img src="/static/img/umbrella.svg">
+      </div>
+      <div class="window" id="${device.station_id}Window">
+        <div class="windowContent">
+          <div class="left" style="background-image: url('${device.image}');"></div>
+          <div class="right">
+            <div class="titleContainer">
+              <span class="title">${device.title}</span>
+              <span class="address">${device.address}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+
+    const marker = new YMapMarker(
+      {
+        coordinates: device.coords.reverse(),
+      },
+      markerElement
+    );
+
+    map.addChild(marker);
+  }
+}
+
+let activeStationWindow = null;
+
+initMap();
