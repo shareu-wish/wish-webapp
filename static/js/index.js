87d6944d05b828e5f2e28c832b65bb3704d936e4
Mark Meshcheryakov <markzum@yandex.ru>
2024-08-09 14:46:46 +0300

    Краткий туториал сделан, отображение прогноза погоды в первой секции, секция тарифов в процессе.

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+
+function loadWeather() {
+  $.getJSON("https://api.ipify.org?format=json", function (data) {
+    $.getJSON(`http://www.geoplugin.net/json.gp?ip=${data.ip}&lang=ru`, function (data2) {
+        let city = data2.geoplugin_city
+        if (!city) {
+            city = data2.geoplugin_region
+        }
+        $.getJSON(`https://api.open-meteo.com/v1/forecast?latitude=${data2.geoplugin_latitude}&longitude=${data2.geoplugin_longitude}&current=temperature_2m,is_day,weathercode`, function (data3) {
+            $.getJSON("/static/weatherCodes.json", function (weatherCodes) {
+                const weatherCode = data3.current.weathercode
+                const timeOfDay = data3.current.is_day ? "day" : "night"
+                const weather = weatherCodes[weatherCode][timeOfDay]["description"]
+
+                if (city && weather) {
+                    $("#current-weather").show()
+                    $("#current-weather-city").text(city)
+                    $("#current-weather-description").text(weather)
+                }
+                
+            });
+        });
+      });
+  });
+}
+
+
+loadWeather();
