9559524e09ecb1b04e48398ef7d34de4e693412d
Mark Meshcheryakov <markzum@yandex.ru>
2024-08-22 14:16:58 +0300

    Сделана секция "Установите аппарат" в "Бизнесу" + произедена SEO-оптимизация

--- static/js/business.js
+++ static/js/business.js
@@ -1,4 +1,42 @@
 
+function sendInstallStationRequest() {
+    const organization = $("#installStationOrganization").val().trim();
+    const city = $("#installStationCity").val().trim();
+    const email = $("#installStationEmail").val().trim();
+    const phone = $("#installStationPhone").val().trim();
+    const text = $("#installStationText").val().trim();
+    if (organization === "" || city === "" || text === "") {
+        return alert("Заполните все обязательные поля!");
+    }
+    if (email === "" && phone === "") {
+        return alert("Укажите хотя бы один контактный способ (email или номер телефона)!");
+    }
+
+    // TODO: Убрать, когда будет готов сервер
+    return alert("К сожалению, эта форма сейчас не работает(");
+
+    $.ajax({
+        url: "/support",
+        type: "POST",
+        data: {
+            organization: organization,
+            city: city,
+            email: email,
+            phone: phone,
+            text: text
+        },
+        success: function (res) {
+            alert("Ваш вопрос отправлен!");
+            $("#installStationOrganization").val("");
+            $("#installStationCity").val("");
+            $("#installStationEmail").val("");
+            $("#installStationPhone").val("");
+            $("#installStationText").val("");
+        }
+    });
+}
+
+
 
 $('#firstSection').mousemove(function(e) {
     const x = e.pageX - this.offsetLeft;
